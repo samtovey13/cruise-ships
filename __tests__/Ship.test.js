48521bb4e5e09349f101b6fa20b49bd6646695f4
Sam Tovey <samtovey13@gmail.com>
2020-07-10 15:59:24 +0100

    added code and tests for edge cases of setSail and dock

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -7,11 +7,12 @@ const portCalais = new Port ('Calais');
 const ports = [portDover, portCalais];
 const itinerary = new Itinerary(ports);
 const ship = new Ship(itinerary);
+const ship2 = new Ship(itinerary);
 
 
 describe('Ship', () => {
     it('creates a new object', () => {
-        expect(new Ship()).toBeInstanceOf(Object);
+        expect(new Ship(itinerary)).toBeInstanceOf(Object);
     });
     it('has an itinerary containing an array of ports', () => {
         expect(ship.itinerary).toEqual(itinerary);
@@ -19,11 +20,7 @@ describe('Ship', () => {
     it('has a previous port property set to null', () => {
         expect(ship.previousPort).toBe(null);
     });
-    it('has a current port property set to null', () => {
-        expect(ship.currentPort).toBe(null);
-    });
-    it('can dock at a port', () => {
-        ship.dock();
+    it('has a current port property set to idex[0] of the ports array', () => {
         expect(ship.currentPort).toEqual(portDover);
     });
     it('can set sail, updating previous port and setting current port to null', () => {
@@ -31,13 +28,20 @@ describe('Ship', () => {
         expect(ship.currentPort).toBeFalsy();
         expect(ship.previousPort).toEqual(portDover);
     });
-    it('can follow an itinerary with two Ports', () => {
-        expect(ship.currentPort).toBeFalsy();
-        expect(ship.previousPort).toEqual(portDover);
+    it('can dock at a port', () => {
         ship.dock();
         expect(ship.currentPort).toEqual(portCalais);
-        expect(ship.previousPort).toEqual(portDover);
-        ship.setSail();
-        expect(ship.previous)
     });
+    it('throws an error if setSail is called but the ship is already sailing', () => {
+        ship2.setSail();
+        expect(() => ship2.setSail()).toThrowError('Already at sea. Dock to arrive at the next port.');
+    });
+    it('throws an error if dock() is called while the ship is already docked', () => {
+        ship2.dock();
+        expect(() => ship2.dock()).toThrowError('Already docked! Set sail to reach the next port.');
+    });
+    it('throws an error if setSail is called but the ship has reached the end of the itinerary', () => {
+        expect(() => ship2.setSail()).toThrowError('End of itinerary reached');
+    });
+
 });
--- src/Ship.js
+++ src/Ship.js
@@ -1,18 +1,27 @@
 function Ship(itinerary) {
     this.itinerary = itinerary;
-    this.currentPort = null;
+    this.currentPort = itinerary.ports[0];
     this.previousPort = null;
-    this.count = 0;
+    this.portsVisited = 1;      // the starting port is included in the count
 };
 
 Ship.prototype.setSail = function() {
+    if (this.portsVisited >= this.itinerary.ports.length) {
+        throw new Error('End of itinerary reached');
+    }
+    if (!this.currentPort) {
+        throw new Error('Already at sea. Dock to arrive at the next port.')
+    }
     this.previousPort = this.currentPort;
     this.currentPort = null;
 };
 
 Ship.prototype.dock = function() {
-    this.currentPort = this.itinerary.ports[this.count];
-    this.count += 1;
+    if (this.currentPort) {
+        throw new Error('Already docked! Set sail to reach the next port.');
+    }
+    this.currentPort = this.itinerary.ports[this.portsVisited];
+    this.portsVisited += 1;
 };
 
 
